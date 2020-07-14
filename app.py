e560b57f8911a63dd541e70a8fda0a8cf1896379
Vaibhav <64318916+vb47@users.noreply.github.com>
2020-07-14 07:11:52 +0530

    Modified everything

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "Templates")
 
 @app.route("/")
 def index():
@@ -9,17 +9,24 @@ def index():
 
 @app.route("/HomePage")
 def HomePage():
-    return render_template("main_template.html", Headline="Alzheimer Project Analysis")
+    return render_template("home.html")
 
 @app.route("/Surveys")
 def Surveys():
-    return render_template("main_template.html", Headline ="Surveys")
+    return render_template("survey.html")
 
 @app.route("/Results")
 def Results():
-    return render_template("main_template.html", Headline ="Results")
+    return render_template("result.html")
 
 @app.route("/Help")
 def Help():
-    return render_template("main_template.html", Headline ="Support")
+    return render_template("help.html")
 
+@app.route("/Feedback")
+def Feedback():
+    return render_template("feedback.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
